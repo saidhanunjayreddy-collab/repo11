3c2145c5d3e7e012305fee4a7c240e0dcf0be5b3
Sai Reddy <saidhanunjayreddy@gmail.com>
2026-02-23 09:33:10 +0530

    subnet block

--- vnet.tf
+++ vnet.tf
@@ -5,3 +5,11 @@ resource "azurerm_virtual_network" "vnet" {
     location = azurerm_resource_group.rg.location
   
 }
+
+resource "azurerm_subnet" "subnet" {
+    name = "saireddy-subnet"
+    address_prefixes = {"10.1.1.0/24"}
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+  
+}
